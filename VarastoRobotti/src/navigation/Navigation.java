088f52713ec3f4e75f1d610dbc7441ee21cd651f
Eero <eerotuu@metropolia.fi>
2018-09-26 21:08:40 +0300

    Navi, Orders muokattu, MakeTransfer lisätty.
    Implementoitu metodit navigation luokkaan, korjattu orders luokka ja
    siirretty connections kansioon, toteutettu MakeTranfer.

--- VarastoRobotti/src/navigation/Navigation.java
+++ VarastoRobotti/src/navigation/Navigation.java
@@ -6,6 +6,7 @@ import lejos.robotics.chassis.*;
 import lejos.robotics.localization.PoseProvider;
 import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.*;
+import lejos.robotics.pathfinding.*;
 
 public class Navigation {
 
@@ -24,20 +25,38 @@ public class Navigation {
 	private Navigator navi = new Navigator(pilot, poseProvider);
 	private LineMap map = null;
 	
-	public Navigation() {
-		
+	public Navigation(Pose startPose) {
+		poseProvider.setPose(startPose);
 	}
 	
-	public void executePath(Waypoint targetWaypoint) {
-		//TODO implement method
+	public void executePath(Waypoint targetWaypoint, boolean store) {
+		Pose pose = poseProvider.getPose();
+		navi.followPath(findPath(pose, targetWaypoint, store));
+		navi.waitForStop();
+		navi.clearPath();
 	}
 	
 	public Pose getPose() {
-		//TODO implement method
-		return null;
+		return poseProvider.getPose();
 	}
 	
 	public void setLineMap(LineMap map) {
 		this.map = map;
+	}
+	
+	//TODO requires testing
+	private Path findPath(Pose pose, Waypoint targetWaypoint, boolean store) {
+		Waypoint wp = null;
+		Path path = new Path();
+		
+		if(store) {
+			wp = new Waypoint(pose.getY(), (float)targetWaypoint.getX());
+		} else {
+			wp = new Waypoint(pose.getX(), (float)targetWaypoint.getY());
+		}
+		
+		path.add(wp);
+		path.add(targetWaypoint);
+		return path;
 	}
 }
