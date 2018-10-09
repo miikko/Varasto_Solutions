00cce4ed27375325b8b265f200049e9d6eb298bb
eerotuu <eerotuu@metropolia.fi>
2018-10-09 16:47:58 +0300

    adjusted diameters and offsets for robot2

--- VarastoRobotti2/src/navigation2/Navigation2.java
+++ VarastoRobotti2/src/navigation2/Navigation2.java
@@ -10,11 +10,11 @@ import lejos.robotics.pathfinding.*;
 
 public class Navigation2 {
 
-	private EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
-	private EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+	private EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+	private EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.C);
 
-	private Wheel leftWheel = WheeledChassis.modelWheel(leftMotor, 1.72).offset(6.25).invert(true);	
-	private Wheel rightWheel = WheeledChassis.modelWheel(rightMotor, 1.72).offset(-6.25).invert(true);
+	private Wheel leftWheel = WheeledChassis.modelWheel(leftMotor, 1.89).offset(6.6).invert(true);	
+	private Wheel rightWheel = WheeledChassis.modelWheel(rightMotor, 1.91).offset(-6.6).invert(true);
 	
 	private Chassis chassis = new WheeledChassis(new Wheel[] {leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
 	private MovePilot pilot = new MovePilot(chassis);
@@ -24,6 +24,10 @@ public class Navigation2 {
 	
 	public Navigation2(Pose startPose) {
 		poseProvider.setPose(startPose);
+		//pilot.rotate(360);
+		//pilot.rotate(-360);
+		executePath(new Waypoint(32, -28, 0),false);
+		executePath(new Waypoint(0, 0, 0),false);
 
 	}
 	
@@ -45,14 +49,14 @@ public class Navigation2 {
 	private Path findPath(Pose pose, Waypoint targetWaypoint, boolean store) {
 		Waypoint wp = null;
 		Path path = new Path();
-		
+		/*
 		if(store) {
 			wp = new Waypoint(pose.getX(), targetWaypoint.getY());
 		} else {
 			wp = new Waypoint(targetWaypoint.getX(), pose.getY());
 		}
-		
-		path.add(wp);
+		*/
+		//path.add(wp);
 		path.add(targetWaypoint);
 		return path;
 	}
